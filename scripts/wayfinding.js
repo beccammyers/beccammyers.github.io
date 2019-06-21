e10bbc8eb98bcf4f2b76d98f96829bc19dc30b13
Dustin Myers <mye15011@byui.edu>
2019-06-20 20:39:37 -0600

    a

--- scripts/wayfinding.js
+++ scripts/wayfinding.js
@@ -3,13 +3,14 @@ function setActiveLink() {
     urlString = document.location.href;
     urlArray = urlString.split('/');
     pageHREF = urlArray[urlArray.length - 1];
-
     if (pageHREF !== "") {
         menu = document.querySelectorAll('ul#topnav li a');
         for (i = 0; i < menu.length; i++) {
             currentURL = (menu[i].getAttribute('href'));
+            HREFArray = currentURL.split('/');
+            currentHREF = HREFArray[HREFArray.length - 1];
             menu[i].parentNode.className = '';
-            if (currentURL === pageHREF) {
+            if (currentHREF === pageHREF) {
                 menu[i].parentNode.className = 'active';
             }
         }
